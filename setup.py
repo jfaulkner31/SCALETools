811a95e5e956089e3abae81d0618353c089266cb
johnuthin <jfaulkner31@gatech.edu>
2025-07-23 09:11:38 -0400

    added dependencies

--- setup.py
+++ setup.py
@@ -4,5 +4,7 @@ setup(
   name='SCALEDepleter',
   version='0.1',
   packages=find_packages(),
-  install_requires=[] # add any dependencies here
+  install_requires=["numpy>=1.26.4",
+                    "pandas>=2.2.2",
+                    "mpi4py>=4.0.0"] # add any dependencies here
 )
